3c7be1cf48172bedd9a5a37e2c090111adcfb5a0
NDG-Sterben <ltnk28102002@gmail.com>
2023-08-13 22:28:54 +0700

    update slider and create navigation in HomePage Frame

---
+++ src/main/java/com/ndg/entities/Navigation.java
@@ -0,0 +1,5 @@
+package com.ndg.entities;
+
+public class Navigation {
+
+}
--- src/main/java/com/ndg/entities/Slider.java
+++ src/main/java/com/ndg/entities/Slider.java
@@ -8,58 +8,43 @@ import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 public class Slider extends Thread implements IApplication {
-    private final JLabel lbPLetter, lbTLetter, lbILetter, lbT1Letter;
-    private final int maxWidth = 20;
+    private final JLabel lbPTIT, lbPTIT1;
+    private final int maxWidth, maxHeight;
 
     public Slider(@NotNull JPanel panel) {
-        lbPLetter = new JLabel("P");
-        lbPLetter.setFont(new Font(fontName, Font.BOLD, fontSize + 12));
-        lbPLetter.setForeground(Color.white);
-        lbPLetter.setBounds(panel.getWidth() / 2 - 2 * maxWidth, 0, maxWidth, panel.getHeight());
-        lbPLetter.setHorizontalAlignment(SwingConstants.CENTER);
+        maxWidth = panel.getWidth();
+        maxHeight = panel.getHeight();
 
-        lbTLetter = new JLabel("T");
-        lbTLetter.setFont(new Font(fontName, Font.BOLD, fontSize + 12));
-        lbTLetter.setForeground(Color.white);
-        lbTLetter.setBounds(lbPLetter.getX() + maxWidth, 0, maxWidth, panel.getHeight());
-        lbTLetter.setHorizontalAlignment(SwingConstants.CENTER);
+        lbPTIT = new JLabel("PTIT - Posts and Telecommunications Institute of Technology");
+        lbPTIT.setFont(new Font(fontName, Font.BOLD, fontSize + 12));
+        lbPTIT.setBounds(panel.getX(), panel.getY(), panel.getWidth(), panel.getHeight());
+        lbPTIT.setForeground(Color.white);
+        lbPTIT.setHorizontalAlignment(SwingConstants.CENTER);
 
-        lbILetter = new JLabel("I");
-        lbILetter.setFont(new Font(fontName, Font.BOLD, fontSize + 12));
-        lbILetter.setForeground(Color.white);
-        lbILetter.setBounds(lbTLetter.getX() + maxWidth, 0, maxWidth, panel.getHeight());
-        lbILetter.setHorizontalAlignment(SwingConstants.CENTER);
+        lbPTIT1 = new JLabel("PTIT - Posts and Telecommunications Institute of Technology");
+        lbPTIT1.setFont(new Font(fontName, Font.BOLD, fontSize + 12));
+        lbPTIT1.setBounds(maxWidth, panel.getY(), panel.getWidth(), panel.getHeight());
+        lbPTIT1.setForeground(Color.white);
+        lbPTIT1.setHorizontalAlignment(SwingConstants.CENTER);
 
-        lbT1Letter = new JLabel("T");
-        lbT1Letter.setFont(new Font(fontName, Font.BOLD, fontSize + 12));
-        lbT1Letter.setForeground(Color.white);
-        lbT1Letter.setBounds(lbILetter.getX() + maxWidth, 0, maxWidth, panel.getHeight());
-        lbT1Letter.setHorizontalAlignment(SwingConstants.CENTER);
-
-        panel.add(lbPLetter);
-        panel.add(lbTLetter);
-        panel.add(lbILetter);
-        panel.add(lbT1Letter);
+        panel.add(lbPTIT);
+        panel.add(lbPTIT1);
     }
 
     @Override
     public void run() {
         while (true) {
-            lbPLetter.setBounds(lbPLetter.getX() - 1, 0, maxWidth, 50);
-            lbTLetter.setBounds(lbTLetter.getX() - 1, 0, maxWidth, 50);
-            lbILetter.setBounds(lbILetter.getX() - 1, 0, maxWidth, 50);
-            lbT1Letter.setBounds(lbT1Letter.getX() - 1, 0, maxWidth, 50);
+            lbPTIT.setBounds(lbPTIT.getX() - 1, lbPTIT.getY(), maxWidth, maxHeight);
+            lbPTIT1.setBounds(lbPTIT1.getX() - 1, lbPTIT1.getY(), maxWidth, maxHeight);
 
             try {
-                TimeUnit.MILLISECONDS.sleep(10);
+                TimeUnit.MILLISECONDS.sleep(20);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
 
-            if (lbPLetter.getX() < -(maxWidth)) lbPLetter.setBounds(width, 0, maxWidth, 50);
-            if (lbTLetter.getX() < -(maxWidth)) lbTLetter.setBounds(width, 0, maxWidth, 50);
-            if (lbILetter.getX() < -(maxWidth)) lbILetter.setBounds(width, 0, maxWidth, 50);
-            if (lbT1Letter.getX() < -(maxWidth)) lbT1Letter.setBounds(width, 0, maxWidth, 50);
+            if (lbPTIT.getX() <= -maxWidth) lbPTIT.setBounds(maxWidth, lbPTIT.getY(), maxWidth, maxHeight);
+            if (lbPTIT1.getX() <= -maxWidth) lbPTIT1.setBounds(maxWidth, lbPTIT.getY(), maxWidth, maxHeight);
         }
     }
 }
